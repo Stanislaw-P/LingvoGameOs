e9e0f624dfb5b0aa427a63a05c9058b1d2b987ec
AmletixQ <nazarovamridin308@gmail.com>
2025-03-19 18:27:08 +0300

    Fix test model & create for all constructor

--- Models/GroupModel.cs
+++ Models/GroupModel.cs
@@ -3,40 +3,92 @@ namespace LingvoGameOs.Models;
 class User
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Email { get; set; }
-    public required string Password { get; set; }
-    public required string Image { get; set; }
-    public required string Link { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Image { get; set; }
+    public string Link { get; set; }
+
+    public User(int id, string name, string email, string password, string image, string link)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Password = password;
+        Image = image;
+        Link = link;
+    }
 }
 
 class Teacher : User
 {
-    public required GroupModel[] Groups { get; set; }
+    public List<GroupModel> Groups { get; set; }
+
+    public Teacher(int id, string name, string email, string password, string image, string link)
+        : base(id, name, email, password, image, link)
+    {
+        Groups = [];
+    }
 }
 
 class Player : User
 {
-    public required GroupModel[] Groups { get; set; }
+    public List<GroupModel> Groups { get; set; }
+
+    public Player(int id, string name, string email, string password, string image, string link)
+        : base(id, name, email, password, image, link)
+    {
+        Groups = [];
+    }
 }
 
 class Game
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Description { get; set; }
-    public required string Image { get; set; }
-    public required string Link { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public string Link { get; set; }
+
+    public Game(int id, string name, string description, string image, string link)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Image = image;
+        Link = link;
+    }
 }
 
 class GroupModel
 {
     public int Id { get; set; }
-    public required Teacher Teacher { get; set; }
-    public required Player[] Players { get; set; }
-    public required Game Game { get; set; }
-    public required string Name { get; set; }
-    public required string Description { get; set; }
-    public required string Image { get; set; }
-    public required string Link { get; set; }
+    public Teacher Teacher { get; set; }
+    public List<Player> Players { get; set; }
+    public Game Game { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public string Link { get; set; }
+
+    public GroupModel(
+        int id,
+        Teacher teacher,
+        List<Player> players,
+        Game game,
+        string name,
+        string description,
+        string image,
+        string link
+    )
+    {
+        Id = id;
+        Teacher = teacher;
+        Players = players;
+        Game = game;
+        Name = name;
+        Description = description;
+        Image = image;
+        Link = link;
+    }
 }
