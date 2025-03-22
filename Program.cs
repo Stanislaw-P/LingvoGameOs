8f7379713c0b0c0ac94b5099c2dc9a1e325b247d
Stas Persaev <persaev10@mail.ru>
2025-03-22 14:18:36 +0300

    Добавил базу данных

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using LingvoGame.Db;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Настройка строки подключения из appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DatabaseContext>(options =>
+	options.UseSqlite(connectionString)); // Используем строку подключения
+
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
