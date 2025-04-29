ee74d62a3a9342b9982125fc6c1f362f0535da53
David Khugaev <ddkhugaev@gmail.com>
2025-04-29 12:29:13 +0300

    Добавлено свойство ссылки на картинку в DevUser

---
+++ LingvoGameOs.Db/Migrations/20250429092548_UpdateDevUsersTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LingvoGameOs.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDevUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "DevUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "DevUsers");
+        }
+    }
+}
--- LingvoGameOs.Db/Models/DevUser.cs
+++ LingvoGameOs.Db/Models/DevUser.cs
@@ -14,6 +14,7 @@ namespace LingvoGameOs.Db.Models
         public string Login { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ImagePath { get; set; }
         public List<Game> Games { get; set; } 
     }
 }
