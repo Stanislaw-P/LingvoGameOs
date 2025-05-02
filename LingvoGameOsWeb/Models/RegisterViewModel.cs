929bdadab48142ab4b568c4403f777cc2a1fe69d
David Khugaev <ddkhugaev@gmail.com>
2025-05-02 16:04:46 +0300

    Добавил модели и представления входа и регистрации Подключил скрипты валидации в шаблон

---
+++ LingvoGameOsWeb/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LingvoGameOs.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
