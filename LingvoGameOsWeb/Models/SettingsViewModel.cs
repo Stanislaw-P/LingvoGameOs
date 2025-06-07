caf938cd5b8e207039021a071ea9f0cfcd6be2b5
David Khugaev <ddkhugaev@gmail.com>
2025-06-07 22:07:46 +0300

    Сделана страница настройки профиля (еще доработаю) Картинку пока менять нельзя UserViewModel изменен и теперь используется для страницы настройки Немного поменял просто профиль

---
+++ LingvoGameOsWeb/Models/SettingsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LingvoGameOs.Models
+{
+    public class SettingsViewModel
+    {
+        [Required(ErrorMessage = "Введите логин")]
+        [EmailAddress(ErrorMessage = "Введите корректный email")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите фамилию")]
+        public string Surname { get; set; }
+
+        public string? Description { get; set; }
+
+        //[Required(ErrorMessage = "Введите пароль")]
+        //[StringLength(100, MinimumLength = 8, ErrorMessage = "Длина пароля должна быть от 8 до 100 символов")]
+        //public string Password { get; set; }
+
+        //[Required(ErrorMessage = "Подтвердите пароль")]
+        //[StringLength(100, MinimumLength = 8, ErrorMessage = "Длина пароля должна быть от 8 до 100 символов")]
+        //[Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        //public string ConfirmPassword { get; set; }
+    }
+}
