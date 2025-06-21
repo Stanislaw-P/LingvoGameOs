b1e8f4bc72e5d6b76b76c106c4bdcd7417fe4267
David Khugaev <ddkhugaev@gmail.com>
2025-06-21 21:19:55 +0300

    Тестировал валидацию

--- LingvoGameOsWeb/Models/AddGameViewModel.cs
+++ LingvoGameOsWeb/Models/AddGameViewModel.cs
@@ -5,40 +5,40 @@ namespace LingvoGameOs.Models
 {
     public class AddGameViewModel
     {
-        [Required(ErrorMessage ="Обязательное поле")]
+        [Required(ErrorMessage ="Обязательное поле (я из модели)")]
         [MaxLength(21, ErrorMessage ="Максимальная длина 21 символ")]
         [MinLength(4, ErrorMessage ="Минимальная длина 4 символа")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "Обязательное поле")]
+        [Required(ErrorMessage = "Обязательное поле (я из модели)")]
         [MaxLength(200, ErrorMessage = "Максимальная длина 200 символов")]
         [MinLength(10, ErrorMessage = "Минимальная длина 10 символов")]
         public string Description { get; set; }
         
-        [Required(ErrorMessage ="Обязательное поле")]
+        [Required(ErrorMessage = "Обязательное поле (я из модели)")]
         [MaxLength(550, ErrorMessage = "Максимальная длина 550 символов")]
         [MinLength(10, ErrorMessage = "Минимальная длина 10 символов")]
         public string Rules { get; set; }
         
-        [Required(ErrorMessage ="Обязательное поле")]
+        [Required(ErrorMessage = "Обязательное поле (я из модели)")]
         public string LanguageLevel { get; set; }
         
-        [Required(ErrorMessage ="Обязательное поле")]
+        [Required(ErrorMessage = "Обязательное поле (я из модели)")]
         public string SkillsLearning { get; set; }
 
-        [Required(ErrorMessage = "Обязательное поле")]
+        [Required(ErrorMessage = "Обязательное поле (я из модели)")]
         public IFormFile CoverImage { get; set; }
 
-        [Required(ErrorMessage ="Обязательное поле")]
+        [Required(ErrorMessage = "Обязательное поле (я из модели)")]
         public IFormFile[] UploadedImages { get; set; }
 
-        [Required(ErrorMessage = "Обязательное поле")]
+        [Required(ErrorMessage = "Обязательное поле (я из модели)")]
         public string VideoUrl { get; set; }
 
         public string? GameURL { get; set; }
         public IFormFile? UploadedGame { get; set; }
 
-        [Required(ErrorMessage ="Обязательное поле")]
+        [Required(ErrorMessage = "Обязательное поле (я из модели)")]
         public string GamePlatform { get; set; }
     }
 }
