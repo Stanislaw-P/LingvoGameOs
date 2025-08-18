5c233eb46186620204bef6a3f96bcba91f544c23
Stas Persaev <persaev10@mail.ru>
2025-08-18 13:05:09 +0300

    Реализовал работу с загрузкой и превью обложки игры на модерации при редактировании админом. Также это чекпоинт, перед тем как начать играть с js

---
+++ LingvoGameOsWeb/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using LingvoGameOs.Areas.Admin.Models;
+using LingvoGameOs.Db;
+using LingvoGameOs.Db.Models;
+using LingvoGameOs.Helpers;
+using LingvoGameOs.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Reflection.Metadata;
+
+namespace LingvoGameOs.Areas.Admin.Controllers
+{
+    [Area(Constants.AdminRoleName)]
+    [Authorize(Roles = Constants.AdminRoleName)]
+    public class HomeController : Controller
+    {
+        readonly UserManager<User> _userManager;
+        readonly IGamesRepository _gamesRepository;
+        readonly IPendingGamesRepository _pendingGamesRepository;
+
+        public HomeController(UserManager<User> userManager, IGamesRepository gamesRepository, IPendingGamesRepository pendingGamesRepository)
+        {
+            _userManager = userManager;
+            _gamesRepository = gamesRepository;
+            _pendingGamesRepository = pendingGamesRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var adminUser = await _userManager.GetUserAsync(User);
+            if (adminUser == null)
+                return NotFound();
+
+            var existingGames = await _gamesRepository.GetAllAsync();
+            var pendingGames = await _pendingGamesRepository.GetAllAsync();
+            var adminUserViewModel = new AdminViewModel
+            {
+                Id = adminUser.Id,
+                Name = adminUser.Name,
+                Surname = adminUser.Surname,
+                UserName = adminUser.UserName!,
+                Description = adminUser.Description,
+                AvatarImgPath = adminUser.AvatarImgPath,
+                ExistingDevGames = existingGames,
+                PendingGames = pendingGames
+            };
+
+            return View(adminUserViewModel);
+        }
+    }
+}
--- LingvoGameOsWeb/Areas/Admin/Controllers/PendingGamesController.cs
+++ LingvoGameOsWeb/Areas/Admin/Controllers/PendingGamesController.cs
@@ -2,58 +2,30 @@
 using LingvoGameOs.Db;
 using LingvoGameOs.Db.Models;
 using LingvoGameOs.Helpers;
-using LingvoGameOs.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.IO;
-using System.Reflection.Metadata;
 
 namespace LingvoGameOs.Areas.Admin.Controllers
 {
     [Area(Constants.AdminRoleName)]
     [Authorize(Roles = Constants.AdminRoleName)]
-    public class GamesController : Controller
+    public class PendingGamesController : Controller
     {
         readonly UserManager<User> _userManager;
-        readonly IGamesRepository _gamesRepository;
         readonly IPendingGamesRepository _pendingGamesRepository;
         readonly ISkillsLearningRepository _skillsLearningRepository;
         readonly FileProvider _fileProvider;
 
-        public GamesController(UserManager<User> userManager, IGamesRepository gamesRepository, IPendingGamesRepository pendingGamesRepository, ISkillsLearningRepository skillsLearningRepository, IWebHostEnvironment appEnvironment)
+        public PendingGamesController(UserManager<User> userManager, IPendingGamesRepository pendingGamesRepository, ISkillsLearningRepository skillsLearningRepository, IWebHostEnvironment appEnvironment)
         {
             _userManager = userManager;
-            _gamesRepository = gamesRepository;
             _pendingGamesRepository = pendingGamesRepository;
             _skillsLearningRepository = skillsLearningRepository;
             _fileProvider = new FileProvider(appEnvironment);
         }
 
-        public async Task<IActionResult> Index()
-        {
-            var adminUser = await _userManager.GetUserAsync(User);
-            if (adminUser == null)
-                return NotFound();
-
-            var existingGames = await _gamesRepository.GetAllAsync();
-            var pendingGames = await _pendingGamesRepository.GetAllAsync();
-            var adminUserViewModel = new AdminViewModel
-            {
-                Id = adminUser.Id,
-                Name = adminUser.Name,
-                Surname = adminUser.Surname,
-                UserName = adminUser.UserName!,
-                Description = adminUser.Description,
-                AvatarImgPath = adminUser.AvatarImgPath,
-                ExistingDevGames = existingGames,
-                PendingGames = pendingGames
-            };
-
-            return View(adminUserViewModel);
-        }
-
-        public async Task<IActionResult> PendingDetails(int gameId)
+        public async Task<IActionResult> Details(int gameId)
         {
             var existingGame = await _pendingGamesRepository.TryGetByIdAsync(gameId);
             if (existingGame == null)
@@ -86,6 +58,18 @@ namespace LingvoGameOs.Areas.Admin.Controllers
                 LanguageLevel = existingGame.LanguageLevel.Name,
                 VideoUrl = existingGame.VideoUrl ?? "Video doesn't exist"
             });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Details([FromForm] EditGameViewModel editGame)
+        {
+            return null;
+        }
+
+        public void DeleteGameImg(string imgPath)
+        {
+            if (System.IO.File.Exists(imgPath))
+                System.IO.File.Delete(imgPath);
         }
     }
 }
