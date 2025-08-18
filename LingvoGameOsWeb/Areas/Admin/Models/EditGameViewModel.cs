87540cac1789ce1561d0927832b696dae302b7e4
Stas Persaev <persaev10@mail.ru>
2025-08-18 13:05:09 +0300

    Реализовал работу с загрузкой и превью обложки игры на модерации при редактировании админом. Также это чекпоинт, перед тем как начать играть с js

--- LingvoGameOsWeb/Areas/Admin/Models/EditGameViewModel.cs
+++ LingvoGameOsWeb/Areas/Admin/Models/EditGameViewModel.cs
@@ -8,41 +8,41 @@ namespace LingvoGameOs.Areas.Admin.Models
         //[Required(ErrorMessage ="Обязательное поле")]
         //[MaxLength(21, ErrorMessage ="Максимальная длина 21 символ")]
         //[MinLength(4, ErrorMessage ="Минимальная длина 4 символа")]
-        public string Title { get; set; }
+        public string Title { get; set; } = null!;
 
         //[Required(ErrorMessage = "Обязательное поле")]
         //[MaxLength(200, ErrorMessage = "Максимальная длина 200 символов")]
         //[MinLength(10, ErrorMessage = "Минимальная длина 10 символов")]
-        public string Description { get; set; }
+        public string Description { get; set; } = null!;
 
         //[Required(ErrorMessage = "Обязательное поле")]
         //[MaxLength(550, ErrorMessage = "Максимальная длина 550 символов")]
         //[MinLength(10, ErrorMessage = "Минимальная длина 10 символов")]
-        public string Rules { get; set; }
+        public string Rules { get; set; } = null!;
 
         //[Required(ErrorMessage = "Обязательное поле")]
-        public string LanguageLevel { get; set; }
+        public string LanguageLevel { get; set; } = null!;
 
         //[Required(ErrorMessage = "Обязательное поле")]
         public List<string> SkillsLearning { get; set; }
 
-        public string CoverImagePath { get; set; }
+        public string? CoverImagePath { get; set; }
         //[Required(ErrorMessage = "Обязательное поле")]
-        public IFormFile CoverImage { get; set; }
+        public IFormFile CoverImage { get; set; } = null!;
         public FileInfo? CoverImageInfo { get; set; }
 
-        public List<ImageFileInfo> ImagesFilesInfo { get; set; }
+        public List<ImageFileInfo> ImagesFilesInfo { get; set; } = null!;
         //[Required(ErrorMessage = "Обязательное поле")]
-        public IFormFile[] UploadedImages { get; set; }
+        public IFormFile[] UploadedImages { get; set; } = null!;
 
         //[Required(ErrorMessage = "Обязательное поле")]
-        public string VideoUrl { get; set; }
+        public string VideoUrl { get; set; } = null!;
 
         public string? GameURL { get; set; }
         public FileInfo? GameFileInfo { get; set; }
 
         //[Required(ErrorMessage = "Обязательное поле")]
-        public string GamePlatform { get; set; }
+        public string GamePlatform { get; set; } = null!;
         public DateTime DispatchDate { get; set; }
         public DateTime PublicationDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
