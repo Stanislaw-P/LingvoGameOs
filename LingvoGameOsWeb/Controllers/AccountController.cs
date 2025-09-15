50de31c254bb11bc12234a7fdfa5daf244c714e1
Atlant <ddkhugaev@gmail.com>
2025-09-15 23:03:24 +0300

    Добавлена возможность сменить пароль Функция работает, но нуждается в доработке

--- LingvoGameOsWeb/Controllers/AccountController.cs
+++ LingvoGameOsWeb/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using LingvoGameOs.Db;
 using LingvoGameOs.Db.Models;
+using LingvoGameOs.Helpers;
 using LingvoGameOs.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LingvoGameOs.Controllers
 {
@@ -10,11 +12,13 @@ namespace LingvoGameOs.Controllers
     {
         private readonly UserManager<User> userManager;
         private readonly SignInManager<User> signInManager;
+        private readonly EmailService emailService;
 
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, EmailService emailService)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
+            this.emailService = emailService;
         }
 
         public IActionResult Login(string? returnUrl)
@@ -78,6 +82,68 @@ namespace LingvoGameOs.Controllers
         {
             await signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = model.UserName;
+                var user = await userManager.FindByEmailAsync(email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Эта почта не зарегистрирована");
+                    return View(model);
+                }
+                var code = await userManager.GeneratePasswordResetTokenAsync(user);
+                var callBackUrl = Url.Action("ResetPassword", "Account", new
+                {
+                    userId = user.Id,
+                    userEmail = email,
+                    userCode = code
+                }, protocol: HttpContext.Request.Scheme);
+                await emailService.SendEmailAsync(email, "Сброс пароля", $"Для сброса пароля, перейдите <a href='{callBackUrl}'>по ссылке</a>");
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ResetPassword(string userCode, string userEmail)
+        {
+            ViewData["userEmail"] = userEmail;
+            return View(new ResetPasswordViewModel { Code = userCode, UserName = userEmail});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = model.UserName;
+                var user = await userManager.FindByEmailAsync(email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Эта почта не зарегистрирована");
+                    return View(model);
+                }
+                var result = await userManager.ResetPasswordAsync(user, model.Code, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Попробуйте сбросить пароль еще раз");
+                    return View(model);
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ LingvoGameOsWeb/Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LingvoGameOs.Models
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required(ErrorMessage = "Введите email")]
+        [EmailAddress(ErrorMessage = "Введите корректный email")]
+        public string UserName { get; set; }
+    }
+}
