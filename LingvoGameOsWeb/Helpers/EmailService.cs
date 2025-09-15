ed0c0ba693442ef52bba6b96f588c00a51a60c0a
Atlant <ddkhugaev@gmail.com>
2025-09-15 23:03:24 +0300

    Добавлена возможность сменить пароль Функция работает, но нуждается в доработке

--- LingvoGameOsWeb/Helpers/EmailService.cs
+++ LingvoGameOsWeb/Helpers/EmailService.cs
@@ -16,21 +16,21 @@ namespace LingvoGameOs.Helpers
         {
             try
             {
-                var emailMessage = new MimeMessage();
-                emailMessage.From.Add(new MailboxAddress(mailSettings.DisplayName, mailSettings.Mail));
-                emailMessage.To.Add(new MailboxAddress(mailSettings.DisplayName, email));
-                emailMessage.Subject = subject;
-                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-                {
-                    Text = message
-                };
-                using (var client = new SmtpClient())
-                {
-                    await client.ConnectAsync(mailSettings.Host, mailSettings.Port);
-                    await client.AuthenticateAsync(mailSettings.Mail, mailSettings.Password);
-                    await client.SendAsync(emailMessage);
-                    await client.DisconnectAsync(true);
-                }
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(mailSettings.DisplayName, mailSettings.Mail));
+            emailMessage.To.Add(new MailboxAddress(mailSettings.DisplayName, email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(mailSettings.Host, mailSettings.Port);
+                await client.AuthenticateAsync(mailSettings.Mail, mailSettings.Password);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
                 return true;
             }
             catch(SmtpCommandException ex)
