5e1417c2ce7e1705ac093f187020184016d63b30
Atlant <ddkhugaev@gmail.com>
2025-09-16 22:07:14 +0300

    Доработал метод отправки писем Теперь он обрабатывает исключение, связанное с ограничениями из-за спама

--- LingvoGameOsWeb/Helpers/EmailService.cs
+++ LingvoGameOsWeb/Helpers/EmailService.cs
@@ -12,22 +12,31 @@ namespace LingvoGameOs.Helpers
         {
             this.mailSettings = mailSettings.Value;
         }
-        public async Task SendEmailAsync(string email, string subject, string message)
+        public async Task<bool> TrySendEmailAsync(string email, string subject, string message)
         {
-            var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress(mailSettings.DisplayName, mailSettings.Mail));
-            emailMessage.To.Add(new MailboxAddress(mailSettings.DisplayName, email));
-            emailMessage.Subject = subject;
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            try
             {
-                Text = message
-            };
-            using (var client = new SmtpClient())
+                var emailMessage = new MimeMessage();
+                emailMessage.From.Add(new MailboxAddress(mailSettings.DisplayName, mailSettings.Mail));
+                emailMessage.To.Add(new MailboxAddress(mailSettings.DisplayName, email));
+                emailMessage.Subject = subject;
+                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                {
+                    Text = message
+                };
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync(mailSettings.Host, mailSettings.Port);
+                    await client.AuthenticateAsync(mailSettings.Mail, mailSettings.Password);
+                    await client.SendAsync(emailMessage);
+                    await client.DisconnectAsync(true);
+                }
+                return true;
+            }
+            catch(SmtpCommandException ex)
             {
-                await client.ConnectAsync(mailSettings.Host, mailSettings.Port);
-                await client.AuthenticateAsync(mailSettings.Mail, mailSettings.Password);
-                await client.SendAsync(emailMessage);
-                await client.DisconnectAsync(true);
+                Console.WriteLine($"При попытке отправить письмо возникло исключение: {ex.Message}");
+                return false;
             }
         }
     }
