e960c2f1716cb08a1ee46e2249a866c81b45bb10
Stas Persaev <persaev10@mail.ru>
2025-10-03 18:01:20 +0300

    Чиню баги

--- LingvoGameOsWeb/Controllers/ProfileController.cs
+++ LingvoGameOsWeb/Controllers/ProfileController.cs
@@ -14,16 +14,17 @@ namespace LingvoGameOs.Controllers
         private readonly SignInManager<User> signInManager;
         readonly IGamesRepository gamesRepository;
         readonly IPendingGamesRepository _pendingGamesRepository;
+        readonly IFavoriteGamesRepository _favoriteGamesRepository;
         readonly FileProvider fileProvider;
 
-        public ProfileController(UserManager<User> userManager, SignInManager<User> signInManager, IGamesRepository gamesRepository, IPendingGamesRepository pendingGamesRepository, IWebHostEnvironment webHostEnvironment)
+        public ProfileController(UserManager<User> userManager, SignInManager<User> signInManager, IGamesRepository gamesRepository, IPendingGamesRepository pendingGamesRepository, IWebHostEnvironment webHostEnvironment, IFavoriteGamesRepository favoriteGamesRepository)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
             this.gamesRepository = gamesRepository;
             _pendingGamesRepository = pendingGamesRepository;
             this.fileProvider = new FileProvider(webHostEnvironment);
-
+            _favoriteGamesRepository = favoriteGamesRepository;
         }
 
         public async Task<IActionResult> IndexAsync(string userId)
@@ -31,8 +32,9 @@ namespace LingvoGameOs.Controllers
             var user = await userManager.FindByIdAsync(userId);
             if (user != null)
             {
-                var games = await gamesRepository.TryGetUserDevGamesAsync(user);
-                user.DevGames = games;
+                var devGames = await gamesRepository.TryGetUserDevGamesAsync(user);
+               
+                user.DevGames = devGames;
                 var pendingGames = await _pendingGamesRepository.TryGetUserDevGamesAsync(user);
                 user.DevPendingGames = pendingGames;
 
