1b505e8c2e97cd313099c4886e135224f88ac800
Stas Persaev <persaev10@mail.ru>
2025-10-16 22:44:55 +0300

    Передалал кеширование отзывов, вынес их кеширование в отдельный сервис

---
+++ LingvoGameOsWeb/Helpers/ReviewCache.cs
@@ -0,0 +1,43 @@
+﻿
+using LingvoGameOs.Db;
+using Microsoft.Extensions.Caching.Memory;
+using System.Reflection.Metadata;
+
+namespace LingvoGameOs.Helpers
+{
+    public class ReviewCache : BackgroundService
+    {
+        readonly IMemoryCache _memoryCache;
+        readonly IServiceProvider _serviceProvider;
+
+        public ReviewCache(IMemoryCache memoryCache, IServiceProvider serviceProvider)
+        {
+            _memoryCache = memoryCache;
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await CachingReviewsAsync();
+                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
+            }
+        }
+
+        private async Task CachingReviewsAsync()
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var repository = scope.ServiceProvider.GetRequiredService<IReviewsRepository>();
+
+                var reviews = await repository.GetAllAsync();
+
+                if (reviews == null)
+                    return;
+                
+                _memoryCache.Set(Constants.CacheAllReviewsKey, reviews, TimeSpan.FromMinutes(10));
+            }
+        }
+    }
+}
