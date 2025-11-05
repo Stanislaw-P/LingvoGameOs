ab42173993d649db8015c6b3152645fadbaf03fb
Stas Persaev <persaev10@mail.ru>
2025-11-05 20:30:19 +0300

    Добавил web-api проект и написал 3 api функции из ТЗ

---
+++ LingvoGameOs.Db/Migrations/20251105103237_AddTotalPointsToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LingvoGameOs.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalPointsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalScore",
+                table: "AspNetUsers",
+                newName: "TotalPoints");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalPoints",
+                table: "AspNetUsers",
+                newName: "TotalScore");
+        }
+    }
+}
