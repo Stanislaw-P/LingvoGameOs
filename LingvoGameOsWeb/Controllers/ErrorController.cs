344d581eadb4459748321b520d4e815c2bd58963
Istiik <istiik@yandex.ru>
2026-03-03 15:20:44 +0300

    теперь работает нормально 404

---
+++ LingvoGameOsWeb/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LingvoGameOs.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("error/404")]
+        public IActionResult PageNotFound()
+        {
+            Response.StatusCode = 404;
+            return View("PageNotFound"); // Ищет Views/Shared/PageNotFound.cshtml
+        }
+
+        [Route("error/500")]
+        public IActionResult ServerError()
+        {
+            Response.StatusCode = 500;
+            return View("Error");
+        }
+    }
+}
