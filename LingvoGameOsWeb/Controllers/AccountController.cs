b6cf77c091a33da88de505acc0e46153738b5057
ddkhugaev <ddkhugaev@gmail.com>
2026-05-02 15:59:03 +0300

    Подтвержение почты

--- LingvoGameOsWeb/Controllers/AccountController.cs
+++ LingvoGameOsWeb/Controllers/AccountController.cs
@@ -3,12 +3,16 @@ using LingvoGameOs.Db.Models;
 using LingvoGameOs.Helpers;
 using LingvoGameOs.Models;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using Microsoft.Win32;
 using System.Security.Claims;
+using System.Text;
 
 namespace LingvoGameOs.Controllers
 {
@@ -233,6 +237,72 @@ namespace LingvoGameOs.Controllers
                 }
             }
             return View(model);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult ConfirmEmail()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> ConfirmEmailPost()
+        {
+            var user = await userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound("Пользователь не найден");
+            }
+
+            if (await userManager.IsEmailConfirmedAsync(user))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+            var callbackUrl = Url.Action(
+            action: "VerifyEmail",
+            controller: "Account",
+            values: new { userId = user.Id, code = code },
+            protocol: Request.Scheme);
+
+            var result = await emailService.TrySendEmailAsync(user.UserName, "Подтверждение почты", $"Для подтверждения, перейдите <a href='{callbackUrl}'>по ссылке</a>");
+            if (result)
+            {
+                return View("ConfirmEmailSent");
+            }
+            return View("ConfirmEmailError");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VerifyEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var user = await userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"Не удалось загрузить пользователя с ID '{userId}'.");
+            }
+
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+
+            var result = await userManager.ConfirmEmailAsync(user, code);
+
+            if (result.Succeeded)
+            {
+                return View("ConfirmEmailSuccess");
+            }
+
+            return View("ConfirmEmailError");
         }
     }
 }
